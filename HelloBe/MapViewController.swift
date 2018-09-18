98cc9ee1ef44e9752b16b24e77f1b76debdc2143
Daniel Aguiar <dan@Daniels-MacBook-Pro.local>
2018-09-17 21:26:17 -0300

    adicionando configuracoes no mapa

--- HelloBe/MapViewController.swift
+++ HelloBe/MapViewController.swift
@@ -10,85 +10,62 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MapViewController: UIViewController {
-    
-    
-    @IBOutlet weak var searchBar: UISearchBar!
-    
-    @IBOutlet weak var mapView: MKMapView!
-    
-    var locationManage = CLLocationManager()
+class MapViewController: UIViewController, MKMapViewDelegate, UISearchBarDelegate {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManage = CLLocationManager()
+    let number = Int(arc4random_uniform(6))
+    
+    var locations = ["Café","Parque","Cinema","Academia", "Restaurante"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //Mostrar a localizacao do usuario
         mapView.showsUserLocation = true
-        
         //Rastrear a localizacao do usuario
         mapView.userTrackingMode = .follow
-        
         // Definindo o delegate do mapview
         mapView.delegate = self
-        
-        
         //Define a delegate (classe que responde) da SearchBar
         searchBar.delegate = self
-        
-        
         requestAuthorization()
-        
     }
 
-
+    
     
     //Solicitando autorizacao do usuario para o uso da sua localizacao
     func requestAuthorization(){
-        
         // defininco a precisao da localizacao do usuario
         locationManage.desiredAccuracy = kCLLocationAccuracyBest
-        
         // solicitando autorizacao para uso da localizacao com o app em uso
         locationManage.requestWhenInUseAuthorization()
     }
 
-}
-
-
-extension MapViewController: UISearchBarDelegate{
     //Implementando método disparado pelo botao search da search bar
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        
         // retirando o foco da searchBar (Esconder o teclado)
         searchBar.resignFirstResponder()
-        
         // Criando objeto que configura uma pesquisa de pontos de interese
         let request = MKLocalSearchRequest()
-        
         // Configurando a região do mapa onde a pesquisa sera feita
         request.region = mapView.region
-        
         // Definindo o que sera buscado
-        request.naturalLanguageQuery = searchBar.text
-        
+        request.naturalLanguageQuery = "Café"
         // criando objeto que realiza a pesquisa
         let search = MKLocalSearch(request: request)
-        
         // realizando a pesquisa
         search.start { (response, error) in
             if error == nil { //Não teve erro na pesquisa
                 guard let response = response else {return}
-                
                 // remover as anotations previamente adicionadas
-                self.mapView.removeAnnotations(self.mapView.annotations)
-                
+               //self.mapView.removeAnnotations(self.mapView.annotations)
                 // varrer todos os pontos de interesse trazidos pela pesquisa
                 for item in response.mapItems{
                     // criando uma annotation
                     let annotation = MKPointAnnotation()
-                    
                     //definindo a latitude e a longitude da annotation
                     annotation.coordinate = item.placemark.coordinate
-                    
                     // definindo um titulo e subtitulo da annotation
                     annotation.title = item.name
                     annotation.subtitle = item.url?.absoluteString
@@ -99,8 +76,7 @@ extension MapViewController: UISearchBarDelegate{
             }
         }
     }
-}
-extension MapViewController : MKMapViewDelegate{
+    
     
     
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) ->
@@ -130,22 +106,14 @@ extension MapViewController : MKMapViewDelegate{
         directions.calculate { (response, error) in
             if error == nil{//nao deu erro
                 guard let response = response else {return}
-                
                 //recuperando a rota
                 guard let route = response.routes.first else {return}
-                
-                // Nome da rota
-                print(route.name)
-                
-                // Distancia da rota
-                print(route.distance)
                 
                 //Passo a passo da rota
                 for step in route.steps{
                     print(step.distance)
                 }
-                
-                
+
                 //apagar as rotas anteriores
                 self.mapView.removeOverlays(self.mapView.overlays)
                 
@@ -154,10 +122,8 @@ extension MapViewController : MKMapViewDelegate{
             }
         }
     }
+    
 }
-
-
-
 
 
 
